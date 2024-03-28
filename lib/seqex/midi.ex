76d33c43064370bebc437e39316d315ee13aff92
dinocosta <dinojoaocosta@gmail.com>
2024-03-28 23:33:43 +0100

    feat: add function to convert note atom to integer
    - Introduce `Seqex.MIDI.atom_to_note/1` function that converts an atom
    representing a note, into it's integer representation for use in MIDI.
    - Update both `Seqex.MIDI.note_on/3` and `Seqex.MIDI.note_off/2` to
      accept atoms as well as integers for the note argument.
    - Update .formmater.exs to allow a line length of 120 characters.

--- lib/seqex/midi.ex
+++ lib/seqex/midi.ex
@@ -14,6 +14,18 @@ defmodule Seqex.MIDI do
   """
   @type note :: 0..127 | {0..127, velocity()}
 
+  # Base note for which to perform the calculation when transforming atom to integer.
+  # This is the C4 note, so we know we should compare the atom's note scale to 4.
+  @base_note 60
+  @base_scale 4
+
+  # How many semitones to increment the note value by, depending on it's distance to the note C.
+  @note_increment %{"C" => 0, "D" => 2, "E" => 4, "F" => 5, "G" => 7, "A" => 9, "B" => 11}
+
+  # When note is sharp (`"s"`) the note should move up one semitone, when flat (`"b"`) it should move down one
+  # semitone.
+  @accidental_increment %{"s" => 1, "b" => -1}
+
   @spec note_on(%Midiex.OutConn{}, note() | [note()], velocity()) :: %Midiex.OutConn{}
   def note_on(conn, note_or_notes, velocity \\ 100)
 
@@ -24,7 +36,8 @@ defmodule Seqex.MIDI do
     end)
   end
 
-  def note_on(conn, note, velocity), do: Midiex.send_msg(conn, <<0x90, note, velocity>>)
+  def note_on(conn, note, velocity) when is_integer(note), do: Midiex.send_msg(conn, <<0x90, note, velocity>>)
+  def note_on(conn, atom, velocity) when is_atom(atom), do: note_on(conn, atom_to_note(atom), velocity)
 
   @spec note_off(%Midiex.OutConn{}, note() | [note()]) :: %Midiex.OutConn{}
   def note_off(conn, notes) when is_list(notes) do
@@ -34,5 +47,35 @@ defmodule Seqex.MIDI do
     end)
   end
 
-  def note_off(conn, note), do: Midiex.send_msg(conn, <<0x80, note, 0>>)
+  def note_off(conn, note) when is_integer(note), do: Midiex.send_msg(conn, <<0x80, note, 0>>)
+  def note_off(conn, atom) when is_atom(atom), do: note_off(conn, atom_to_note(atom))
+
+  @doc """
+  Convers a note, as atom, to the integer representation of the note for use in MIDI messages.
+
+  ## Examples
+
+      iex> Seqex.MIDI.atom_to_note(:C4)
+      60
+      iex> Seqex.MIDI.atom_to_note(:B5)
+      83
+      iex> Seqex.MIDI.atom_to_note(:Db3)
+      49
+      iex> Seqex.MIDI.atom_to_note(:Cs3)
+      49
+  """
+  @spec atom_to_note(atom()) :: 0..127
+  def atom_to_note(atom) do
+    atom
+    |> Atom.to_string()
+    |> String.codepoints()
+    |> then(fn
+      [note, scale] ->
+        (String.to_integer(scale) - @base_scale) * 12 + @base_note + @note_increment[note]
+
+      [note, accidental, scale] ->
+        (String.to_integer(scale) - @base_scale) * 12 + @base_note + @note_increment[note] +
+          @accidental_increment[accidental]
+    end)
+  end
 end
